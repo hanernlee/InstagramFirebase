500e70970b23831bfcd7e3c51877f2382906fe2e
Christopher Lee <hanernlee@gmail.com>
2017-11-30 23:48:06 +1100

    Refactorize layout constraints

---
+++ InstagramFirebase/InstagramFirebase/Utils/Extensions.swift
@@ -0,0 +1,17 @@
+//
+//  Extensions.swift
+//  InstagramFirebase
+//
+//  Created by Christopher Lee on 30/11/17.
+//  Copyright © 2017 Christopher Lee. All rights reserved.
+//
+
+import UIKit
+
+extension UIColor {
+    static func rgb(red: CGFloat, green: CGFloat, blue: CGFloat) -> UIColor {
+        return UIColor(red: red/255, green: green/255, blue: blue/255, alpha: 1)
+    }
+    
+    
+}
