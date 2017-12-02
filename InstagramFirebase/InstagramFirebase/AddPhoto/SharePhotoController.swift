ad302b9e57b78f4625a95aac98102fffd3664044
Christopher Lee <hanernlee@gmail.com>
2017-12-03 10:19:34 +1100

    Creating photo lists in db

--- InstagramFirebase/InstagramFirebase/AddPhoto/SharePhotoController.swift
+++ InstagramFirebase/InstagramFirebase/AddPhoto/SharePhotoController.swift
@@ -7,6 +7,7 @@
 //
 
 import UIKit
+import Firebase
 
 class SharePhotoController: UIViewController {
     
@@ -59,6 +60,49 @@ class SharePhotoController: UIViewController {
     }
     
     @objc func handleShare() {
+        guard let image = selectedImage else { return }
         
+        guard let uploadData = UIImageJPEGRepresentation(image, 0.5) else { return }
+        
+        navigationItem.rightBarButtonItem?.isEnabled = false
+        
+        let filename = NSUUID().uuidString
+        
+        Storage.storage().reference().child("posts").child(filename).putData(uploadData, metadata: nil) { (metadata, err) in
+            if let err = err {
+                self.navigationItem.rightBarButtonItem?.isEnabled = true
+                print("Failed to upload host image", err)
+                return
+            }
+            
+            guard let imageUrl = metadata?.downloadURL()?.absoluteString else { return }
+            
+            print("Successfully uploaded post imaage:", imageUrl)
+            
+            self.saveToDatabaseWithImageUrl(imageUrl: imageUrl)
+        }
+    }
+    
+    fileprivate func saveToDatabaseWithImageUrl(imageUrl: String) {
+        guard let postImage = selectedImage else { return }
+        guard let caption = textView.text else { return }
+        
+        guard let uid = Auth.auth().currentUser?.uid else { return }
+        
+        let userPostRef = Database.database().reference().child("posts").child(uid)
+        let ref = userPostRef.childByAutoId()
+        
+        let values = ["imageUrl": imageUrl, "caption": caption, "imageWidth": postImage.size.width, "imageHeight": postImage.size.height, "creationDate": Date().timeIntervalSince1970] as [String : Any]
+        
+        ref.updateChildValues(values) { (err, ref) in
+            if let err = err {
+                self.navigationItem.rightBarButtonItem?.isEnabled = true
+                print("Failed to save post to DB", err)
+                return
+            }
+            
+            print("Succesfully saved photo ref to DB")
+            self.dismiss(animated: true, completion: nil)
+        }
     }
 }
