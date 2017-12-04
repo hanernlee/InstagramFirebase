5b1b4d89a4c35ff032c61c3612e188bd05d76bdc
Christopher Lee <hanernlee@gmail.com>
2017-12-04 23:23:41 +1100

    search user controller

---
+++ InstagramFirebase/InstagramFirebase/Search/UserSearchController.swift
@@ -0,0 +1,17 @@
+//
+//  UserSearchController.swift
+//  InstagramFirebase
+//
+//  Created by Christopher Lee on 4/12/17.
+//  Copyright © 2017 Christopher Lee. All rights reserved.
+//
+
+import UIKit
+
+class UserSearchController: UIViewController {
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        
+        collectionView?.backgroundColor = .red
+    }
+}
